ea7fd3de4de582d632f09cd1f9e359fe5c4d9ae0
Prat2030 <pratyushjindal2003@gmail.com>
2022-07-22 12:07:32 +0530

    added customer screen

---
+++ lib/screens/customer_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CustomerScreen extends StatelessWidget {
+  const CustomerScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Customer'),
+      ),
+      body: Center(
+        child: Text('Customer Screen'),
+      ),
+    );
+  }
+}
