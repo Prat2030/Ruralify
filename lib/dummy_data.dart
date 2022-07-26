9cd18c6ab5e68589776e1cbe30bab1c582f678cc
Prat2030 <pratyushjindal2003@gmail.com>
2022-07-26 19:43:50 +0530

    created dummy data to test the app and widgets

---
+++ lib/dummy_data.dart
@@ -0,0 +1,53 @@
+import 'package:ruban_app/models/worker.dart';
+import './models/worker.dart';
+
+var DummyData = [
+  Worker(
+    name: 'Person 1',
+    description: 'This is the description of the person',
+    imageUrl: 'https//www.google.com/',
+    id: 'd34221',
+    phone: '1234567890',
+    price: '345',
+  ),
+  Worker(
+    name: 'Person 1',
+    description: 'This is the description of the person',
+    imageUrl: 'https//www.google.com/',
+    id: 'd34221',
+    phone: '1234567890',
+    price: '345',
+  ),
+  Worker(
+    name: 'Person 1',
+    description: 'This is the description of the person',
+    imageUrl: 'https//www.google.com/',
+    id: 'd34221',
+    phone: '1234567890',
+    price: '345',
+  ),
+  Worker(
+    name: 'Person 1',
+    description: 'This is the description of the person',
+    imageUrl: 'https//www.google.com/',
+    id: 'd34221',
+    phone: '1234567890',
+    price: '345',
+  ),
+  Worker(
+    name: 'Person 1',
+    description: 'This is the description of the person',
+    imageUrl: 'https//www.google.com/',
+    id: 'd34221',
+    phone: '1234567890',
+    price: '345',
+  ),
+  Worker(
+    name: 'Person 1',
+    description: 'This is the description of the person',
+    imageUrl: 'https//www.google.com/',
+    id: 'd34221',
+    phone: '1234567890',
+    price: '345',
+  ),
+];
