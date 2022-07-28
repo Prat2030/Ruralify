32a3f9713fadf8c2c096ede9aaa2bba46c633561
Prat2030 <pratyushjindal2003@gmail.com>
2022-07-28 18:09:55 +0530

    impemented gridtile in workers_tile widget

--- lib/widgets/workers_tile.dart
+++ lib/widgets/workers_tile.dart
@@ -10,32 +10,38 @@ class WorkersTile extends StatelessWidget {
     return InkWell(
       onTap: () {},
       child: Container(
-        padding: const EdgeInsets.all(4),
-        margin: const EdgeInsets.only(top: 8, bottom: 8),
-        child: Container(
-          margin: const EdgeInsets.all(2),
-          padding: const EdgeInsets.all(8),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-            color: Colors.white,
+        margin: const EdgeInsets.all(2),
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.grey.shade400,
+        ),
+        child: GridTile(
+          child: Image(
+            image: NetworkImage(
+                'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+            height: 100,
+            width: double.infinity,
+            fit: BoxFit.cover,
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                'Worker Name',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
+          footer: GridTileBar(
+            backgroundColor: Colors.black45,
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Worker Name',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Text(
-                'Worker Description',
-                style: TextStyle(
-                  fontSize: 16,
+                IconButton(
+                  icon: Icon(Icons.call),
+                  onPressed: () {},
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
