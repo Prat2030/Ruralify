f6f43b6faefde3a92660fdd2d352cf43640c6e99
Prat2030 <pratyushjindal2003@gmail.com>
2022-08-04 19:37:32 +0530

    initiate bookings_item widget

---
+++ lib/widgets/bookings_item.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BookingsItem extends StatelessWidget {
+  const BookingsItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
